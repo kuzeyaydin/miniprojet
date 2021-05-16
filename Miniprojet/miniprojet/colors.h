7fa1be602b263c94fa4ad891b1f84451a177005d
kuzey <kuzeyaydin@gmail.com>
2021-05-16 20:47:49 +0200

    added 2 comments

--- Miniprojet/miniprojet/colors.h
+++ Miniprojet/miniprojet/colors.h
@@ -6,7 +6,9 @@ enum color {
 	RED, GREEN, BLUE, YELLOW, MAGENTA, CYAN, BLACK, WHITE
 };
 
+//chooses targeted color from the selector
 uint8_t target_color(void);
+//lights leds up according to target_color
 void toggle_color_leds(void);
 
 #endif /* COLORS_H_ */
